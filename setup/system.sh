2050f7cd66118651dd5f807ae6e5db9ada346535
Travis <travatine@hotmail.com>
2024-05-12 16:12:17 +0800

    Update system.sh

--- setup/system.sh
+++ setup/system.sh
@@ -382,6 +382,8 @@ cp -f conf/fail2ban/filter.d/* /etc/fail2ban/filter.d/
 # Roundcube for the first time. This causes fail2ban to fail to start. Later
 # scripts will ensure the files exist and then fail2ban is given another
 # restart at the very end of setup.
-restart_service fail2ban
 
-systemctl enable fail2ban
+# defer restart of fail2ban to after initial docker build
+#restart_service fail2ban
+
+#systemctl enable fail2ban
